f6410109738db34ab4b15f57d1ed890907548bcf
Zeis-Marian <Zeis.Marian.Extern@swm.de>
2022-11-15 09:09:38 +0000

    init

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller", "cc/md/Markdown"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, Markdown) {
+        "use strict";
+
+        return Controller.extend("consumenpm.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
